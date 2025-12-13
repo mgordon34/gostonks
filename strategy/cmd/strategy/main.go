197ae9c1ce0963586e4ba5c18a11027b6fe8245e
mgordon34 <coltfanman@gmail.com>
2025-12-13 07:27:27 +0100

    Added strategy service

---
+++ strategy/cmd/strategy/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	"github.com/mgordon34/gostonks/internal/config"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	redisHost := config.Get("REDIS_HOST", "redis")
+	redisPort := config.Get("REDIS_PORT", "6379")
+	addr := fmt.Sprintf("%s:%s", redisHost, redisPort)
+
+	client := redis.NewClient(&redis.Options{Addr: addr})
+	defer client.Close()
+
+	log.Printf("Strategy service listening for candles on redis list 'market' at %s", addr)
+
+	for {
+		values, err := client.BLPop(ctx, 0*time.Second, "market").Result()
+		if err != nil {
+			if errors.Is(err, context.Canceled) || ctx.Err() != nil {
+				log.Printf("Strategy service shutting down: %v", ctx.Err())
+				return
+			}
+			log.Printf("BLPOP error: %v", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		if len(values) == 2 {
+			log.Printf("Received candle payload: %s", values[1])
+			continue
+		}
+		log.Printf("Unexpected BLPOP response: %v", values)
+	}
+}
