8d0a50b398c9d8a8d4ed5bff3b0671e24193f0bf
mgordon34 <coltfanman@gmail.com>
2025-12-16 08:47:53 +0100

    Added more strat functions

--- analysis/internal/strategy/strategy.go
+++ analysis/internal/strategy/strategy.go
@@ -12,14 +12,19 @@ type Strategy interface {
 
 type BarStrategy struct {
 	Name string
+	Market string
 	Symbols []string
 	Lookback int
+	Bars map[string][]candle.Candle
 }
 
-func NewBarStrategy(name string, symbols []string) *BarStrategy {
+func NewBarStrategy(name string, market string, symbols []string, lookback int) *BarStrategy {
 	b := &BarStrategy{
 		Name: name,
+		Market: market,
 		Symbols: symbols,
+		Lookback: lookback,
+		Bars: make(map[string][]candle.Candle),
 	}
 
 	b.initStrategyData()
@@ -29,14 +34,28 @@ func NewBarStrategy(name string, symbols []string) *BarStrategy {
 func (b *BarStrategy) ProcessCandle(c candle.Candle) {
 	for _, symbol := range b.Symbols {
 		if c.Symbol == symbol {
-			log.Printf("Executing step for %s", b.Name)
+			log.Printf("Processing %s candle for %s", c.Symbol, b.Name)
+
+			if err := b.getNCandles(c.Symbol, b.Lookback); err != nil {
+				log.Printf("Failed to get lookback for %s on %x", c.Symbol, c.Timestamp.Format("2006-01-02 15:04:05"))
+			}
 		}
+	}
+}
+
+func (b *BarStrategy) GenerateSignal(c candle.Candle) {
+	for _, symbol := range b.Symbols {
+		log.Printf("Generating signals for %s", symbol)
 	}
 }
 
 func (b *BarStrategy) initStrategyData() {
 	for _, symbol := range b.Symbols {
 		log.Printf("Preloading data for %s", symbol)
-
+		b.getNCandles(symbol, b.Lookback)
 	}
+}
+
+func (b *BarStrategy) getNCandles(symbol string, n int) error {
+	return nil
 }
