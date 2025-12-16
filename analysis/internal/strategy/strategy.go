b8cc4d23b64d7600eebf5f7d90cd5b910d9bbf12
mgordon34 <coltfanman@gmail.com>
2025-12-16 07:03:28 +0100

    Adding strategy init

--- analysis/internal/strategy/strategy.go
+++ analysis/internal/strategy/strategy.go
@@ -3,29 +3,40 @@ package strategy
 import (
 	"log"
 
-	"github.com/mgordon34/gostonks/analysis/internal/candle"
+	"github.com/mgordon34/gostonks/market/cmd/candle"
 )
 
 type Strategy interface {
-	ExecuteStep(c candle.Candle)
+	ProcessCandle(c candle.Candle)
 }
 
 type BarStrategy struct {
 	Name string
 	Symbols []string
+	Lookback int
 }
 
 func NewBarStrategy(name string, symbols []string) *BarStrategy {
-	return &BarStrategy{
+	b := &BarStrategy{
 		Name: name,
 		Symbols: symbols,
 	}
+
+	b.initStrategyData()
+	return b
 }
 
-func (b *BarStrategy) ExecuteStep(c candle.Candle) {
+func (b *BarStrategy) ProcessCandle(c candle.Candle) {
 	for _, symbol := range b.Symbols {
 		if c.Symbol == symbol {
 			log.Printf("Executing step for %s", b.Name)
 		}
+	}
+}
+
+func (b *BarStrategy) initStrategyData() {
+	for _, symbol := range b.Symbols {
+		log.Printf("Preloading data for %s", symbol)
+
 	}
 }
