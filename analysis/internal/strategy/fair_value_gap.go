5ce0981fcae2be5cf74baf6d57c95ebdc3540a45
mgordon34 <coltfanman@gmail.com>
2025-12-24 09:44:03 +0100

    Added processCandle function for gapmanager

--- analysis/internal/strategy/fair_value_gap.go
+++ analysis/internal/strategy/fair_value_gap.go
@@ -9,10 +9,10 @@ import (
 type GapStatus string
 
 const (
-	Open 			GapStatus = "open"
-	PartiallyFilled GapStatus = "partially_filled"
-	Filled 			GapStatus = "filled"
-	Inversed 		GapStatus = "inversed"
+	GapOpen 			GapStatus = "open"
+	GapPartiallyFilled 	GapStatus = "partially_filled"
+	GapFilled 			GapStatus = "filled"
+	GapInversed 		GapStatus = "inversed"
 )
 
 type FairValueGap struct {
@@ -26,29 +26,63 @@ type FairValueGap struct {
 }
 
 type GapManager struct {
+	candles  	[]candle.Candle
 	gaps		[]FairValueGap
 }
 
-func (gm *GapManager) AddGap(fvg FairValueGap) {
-}
-
-func (gm *GapManager) UpdateGaps(candles []candle.Candle) {
-	if len(candles) != 3 {
-		log.Fatalf("UpdateGaps found invalid number of candles: %d", len(candles))
+func (gm *GapManager) ProcessCandle(candle candle.Candle) {
+	gm.candles = append(gm.candles, candle)
+	if len(gm.candles) > 3 {
+		gm.candles = gm.candles[1:]
+		gm.addGapIfExists()
 	}
 
+	gm.updateGaps()
+}
+
+func (gm *GapManager) addGapIfExists() {
+	if len(gm.candles) < 3 {
+		return
+	}
+
+	if gm.candles[0].High < gm.candles[2].Low {
+		gap := FairValueGap{
+			Direction: Buyside,
+			StartPrice: gm.candles[0].High,
+			EndPrice: gm.candles[2].Low,
+			UnfilledPrice: gm.candles[2].Low,
+			Candle: &gm.candles[1],
+			LastAffectedCandle: &gm.candles[1],
+			State: GapOpen,
+		}
+		gm.gaps = append(gm.gaps, gap)
+	} else if gm.candles[0].Low > gm.candles[2].High {
+		gap := FairValueGap{
+			Direction: Sellside,
+			StartPrice: gm.candles[0].Low,
+			EndPrice: gm.candles[2].High,
+			UnfilledPrice: gm.candles[2].High,
+			Candle: &gm.candles[1],
+			LastAffectedCandle: &gm.candles[1],
+			State: GapOpen,
+		}
+		gm.gaps = append(gm.gaps, gap)
+	}
+}
+
+func (gm *GapManager) updateGaps() {
 	// check if gaps have been filled / inversed
 	// lastCandle = candles[2]
 	for i := len(gm.gaps) - 1; i >= 0; i-- {
 		curGap := gm.gaps[i]
 		switch curGap.State {
-		case Open:
+		case GapOpen:
 			if curGap.Direction == Buyside {
 
 			}
-		case PartiallyFilled:
-		case Filled:
-		case Inversed:
+		case GapPartiallyFilled:
+		case GapFilled:
+		case GapInversed:
 		}
 	}
 }
