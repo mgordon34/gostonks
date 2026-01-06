f973754cc46c1b0d5e4b0346ffe37a25c118a7f3
mgordon34 <coltfanman@gmail.com>
2026-01-06 16:30:24 +0100

    Added riskStrategy

---
+++ analysis/internal/risk/risk.go
@@ -0,0 +1,32 @@
+package risk
+
+import (
+	"log"
+	"math"
+)
+
+type RiskStrategy interface {
+	GetQuantityForTrade(entryPrice float64, stopLoss float64) float64
+}
+
+type SimpleRiskStrategy struct {
+	Market			string
+	RiskPercent 	float64
+}
+
+func (rs *SimpleRiskStrategy) GetQuantityForTrade(balance float64, entryPrice float64, stopLoss float64) int {
+	riskDollars := balance * rs.RiskPercent
+	contractCost := math.Abs(entryPrice - stopLoss) * float64(GetRiskPerPoint(rs.Market))
+
+	return int(riskDollars / contractCost)
+}
+
+func GetRiskPerPoint(market string) int {
+	switch market {
+	case "futures":
+		return 20
+	default:
+		log.Fatalf("Unknown market: %s", market)
+		return 0
+	}
+}
